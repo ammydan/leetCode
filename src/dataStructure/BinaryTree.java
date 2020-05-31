7d01b83a50cdbfea369755beec887c7b7fa90af3
creator <772644080@qq.com>
2020-05-31 14:58:05 +0800

    5.31

---
+++ src/dataStructure/BinaryTree.java
@@ -0,0 +1,35 @@
+/***
+ * Definition:
+ *A binary tree is a tree such that
+ * every node has at most 2 children
+ * each node is labeled as being either a left child or a right child.
+ *
+ * Terminology:
+ * 路径(path)：从一个节点走到另一个节点，所经过的节点的顺序排列就称为“路径”
+ * 根(root)：树的顶端称为“根”。一棵树只有一个根，所有节点都只有一条到根的路径。
+ * 子节点(child node)：每个节点都有可能有一条或者多条向下连接其他节点。
+ * 叶子节点(leaves node)：没有子节点的节点。
+ * 子树(subtree)：每个节点都可以作为“子树”的根。
+ * 遍历(traverse)：遍历树以为这要遵循某种特定的顺序访问树中的所有节点。
+ * 层(layer)：一个节点的层数是指从根开始到这个节点有多少“代”。
+ *
+ * ADT：
+ * root; size;
+ * Node{
+ *     left; right;
+ *     data;
+ * }
+ * root()
+ * size()
+ * isEmpty()
+ * parent(node)
+ * children(node)
+ * isInternal(node)
+ * isExternal(node)
+ * ***/
+//public class BinaryTree {
+//    private Class Node<>{
+//
+//    }
+//    private
+//}
